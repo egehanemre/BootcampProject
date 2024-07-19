57c14bd9ab4e14015eec1838c3c38bb62b8a2d0c
aysepelinsecim <670pelin@gmail.com>
2024-07-19 21:32:06 +0300

    yorum satırı hata veren kısıım

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,17 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
-{
-    public void StartGame() 
-    {         
-        UnityEngine.SceneManagement.SceneManager.LoadScene("BattleScene");
-    }
+//public class SceneManager : MonoBehaviour
+//{
+//    public void StartGame() 
+//    {         
+//        UnityEngine.SceneManagement.SceneManager.LoadScene("BattleScene");
+//    }
 
-    public void QuitGame() 
-    {
-        Application.Quit();
-    }   
-}
+//    public void QuitGame() 
+//    {
+//        Application.Quit();
+//    }   
+//}
